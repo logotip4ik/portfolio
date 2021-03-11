27581a21fd573ba9f57fae55637905923a788ce2
logotip4ik <bogdankostyuk12@gmail.com>
2021-03-11 20:16:34 +0200

    update: liitle enhancement to popup

--- src/components/V-Popup.vue
+++ src/components/V-Popup.vue
@@ -1,10 +1,12 @@
 <template>
-  <div :class="{ popup: true, error: !success }" @click="$emit('close')">
-    {{
-      success
-        ? 'Your message is send Successfully!'
-        : 'Something went wrong. Please try again later.'
-    }}
+  <div class="popup__container">
+    <div :class="{ popup: true, error: !success }" @click="$emit('close')">
+      {{
+        success
+          ? 'Your message is send successfully!'
+          : 'Something went wrong. Please try again later.'
+      }}
+    </div>
   </div>
 </template>
 
@@ -23,22 +25,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.popup {
+.popup__container {
   position: fixed;
-  top: 1.5rem;
+  top: 2rem;
   left: 50%;
   transform: translate(-50%);
-  max-width: 500px;
+  padding: 1rem;
+  z-index: 200;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.popup {
+  max-width: 400px;
   padding: 1.5rem 2rem;
   color: #18181e;
-  background: rgba(#79ffe1, 0.6);
-  z-index: 200;
+  text-align: center;
+  background: rgba(#79ffe1, 0.7);
   border-radius: 0.25rem;
   box-shadow: 0 0 5px rgba(#000, 0.3);
 
   &.error {
     background: rgba($color: #ff0000, $alpha: 0.3);
     color: white;
+  }
+}
+
+@media screen and (max-width: 485px) {
+  .popup {
+    // padding: 0.8rem 1.25rem;
   }
 }
 </style>
