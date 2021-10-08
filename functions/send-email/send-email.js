382229ce2eab6c0f883473f32c977e77133cb94a
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-10-08 14:05:19 +0300

    update: more debug on netlify

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -17,56 +17,77 @@ const schema = Joi.object({
     .required(),
 });
 
-async function sendEmail(data) {
-  console.log({ API_DOMAIN: process.env.API_DOMAIN, API_KEY: process.env.API_KEY });
-  return new Promise((resolve, reject) => {
-    const mg = mailgun({ domain: process.env.API_DOMAIN, apiKey: process.env.API_KEY });
-    mg.messages().send(data, (err) => (err ? reject(err) : resolve()));
-  });
-}
+// async function sendEmail(data) {
+//   console.log({ API_DOMAIN: process.env.API_DOMAIN, API_KEY: process.env.API_KEY });
+//   return new Promise((resolve, reject) => {
+//     const mg = mailgun({ domain: process.env.API_DOMAIN, apiKey: process.env.API_KEY });
+//     mg.messages().send(data, (err) => (err ? reject(err) : resolve()));
+//   });
+// }
 
-module.exports.handler = async (event) => {
+module.exports.handler = (event, _, callback) => {
   // eslint-disable-next-line
   if (event.httpMethod !== 'POST')
     // eslint-disable-next-line
-    return {
+    callback(null, {
       statusCode: 400,
       body: JSON.stringify({
         msg: 'method is not allowed',
       }),
-    };
+    });
 
   let body;
   try {
     body = JSON.parse(event.body);
   } catch (error) {
-    return { statusCode: 400, msg: 'Not valid input' };
+    callback(null, { statusCode: 400, msg: 'Not valid input' });
   }
 
   const validBody = schema.validate(body);
-  if (validBody.error) return { statusCode: 400, body: JSON.stringify({ error: validBody.error }) };
+  if (validBody.error)
+    callback(null, { statusCode: 400, body: JSON.stringify({ error: validBody.error }) });
   // eslint-disable-next-line
   if (validBody.warning)
     // eslint-disable-next-line
-    return {
+    callback(null, {
       statusCode: 400,
       body: JSON.stringify({
         warning: validBody.warning,
       }),
-    };
+    });
 
-  await sendEmail({
-    to: 'bogdankostyuk12@gmail.com',
-    text: validBody.value.message,
-    subject: 'New form submission',
-    from: `server@${process.env.API_DOMAIN}`,
-    template: 'from-my-website',
-    'h:X-Mailgun-Variables': JSON.stringify({
-      name: validBody.value.name,
-      email: validBody.value.email,
-      message: validBody.value.message,
-    }),
-  }).catch((err) => console.log({ err, msg: 'something went wrong when sending mail' }));
+  const mg = mailgun({ apiKey: process.env.API_KEY, domain: process.env.API_DOMAIN });
+  mg.messages().send(
+    {
+      to: 'bogdankostyuk12@gmail.com',
+      text: validBody.value.message,
+      subject: 'New form submission',
+      from: `server@${process.env.API_DOMAIN}`,
+      template: 'from-my-website',
+      'h:X-Mailgun-Variables': JSON.stringify({
+        name: validBody.value.name,
+        email: validBody.value.email,
+        message: validBody.value.message,
+      }),
+    },
+    (err) => {
+      if (err) console.log({ msg: 'something went wrong with sending email', err });
+      callback(null, { statusCode: 200, body: JSON.stringify({ msg: 'successfully sent email' }) });
+    },
+  );
 
-  return { statusCode: 200, body: JSON.stringify({ msg: 'successfully sent email' }) };
+  // await sendEmail({
+  //   to: 'bogdankostyuk12@gmail.com',
+  //   text: validBody.value.message,
+  //   subject: 'New form submission',
+  //   from: `server@${process.env.API_DOMAIN}`,
+  //   template: 'from-my-website',
+  //   'h:X-Mailgun-Variables': JSON.stringify({
+  //     name: validBody.value.name,
+  //     email: validBody.value.email,
+  //     message: validBody.value.message,
+  //   }),
+  // }).catch((err) => console.log({ err, msg: 'something went wrong when sending mail' }));
+
+  // return { statusCode: 200, body: JSON.stringify({ msg: 'successfully sent email' }) };
 };
