1d5fca94cf6e00682b5064e0130f44154a6f051f
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-09 21:03:04 +0200

    update: created navbar for the website

---
+++ components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="nav">
+    <NuxtLink to="/projects">Projects</NuxtLink>
+    <NuxtLink to="/posts">Blog</NuxtLink>
+    <NuxtLink to="#">Contact</NuxtLink>
+    <ColorThemeToggle />
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.nav {
+  display: grid;
+  grid-template-columns: repeat(4, min-content);
+  justify-content: end;
+  align-items: center;
+  gap: 3rem;
+
+  width: 100%;
+
+  padding: 2.5rem 2rem;
+
+  a {
+    font-size: 1.5rem;
+  }
+}
+</style>
