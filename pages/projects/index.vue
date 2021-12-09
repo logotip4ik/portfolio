3b8c323838d275e021f2e700efbfd28d9a415db1
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-09 22:10:05 +0200

    update: added boilerplate code just to see if it will work

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1>Hello from projects route</h1>
+  <div>
+    <h1>Hello from projects route</h1>
+    <pre>{{ projects }}</pre>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const projects = await $content('projects').fetch()
+
+    return { projects }
+  },
+}
 </script>
 
 <style></style>
