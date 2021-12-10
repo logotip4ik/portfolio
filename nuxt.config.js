c36f1dd30d12a2a94562a7858df16a29bb5bb95e
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-10 21:47:40 +0200

    update: added smooth scroll plugin to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,10 +18,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/css/global.css'],
+  css: ['~/assets/css/global.css', '~/assets/css/smooth-scroll.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/smooth-scroll.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
