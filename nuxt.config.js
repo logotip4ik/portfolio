600703cffb03b2784a0f2cd622b6d58b02af473f
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-10 22:32:12 +0200

    update: added small global css file in order to padding be the same on all pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/css/global.css', '~/assets/css/smooth-scroll.css'],
+  css: [
+    '~/assets/css/global.css',
+    '~/assets/css/smooth-scroll.css',
+    '~/assets/css/main.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: '~/plugins/smooth-scroll.js', mode: 'client' }],
