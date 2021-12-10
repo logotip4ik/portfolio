5c5682463f54a46bfbf755b7f892f75429b2513f
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-10 18:48:22 +0200

    update: added basic routing feature

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <h1>Hello world from {{ $route.path }}</h1>
+  <div>
+    <h1>Hello world from {{ $route.path }}</h1>
+
+    <hr />
+
+    <ul>
+      <li v-for="post in posts" :key="post.slug">
+        <NuxtLink :to="post.path">
+          {{ post.name }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const posts = await $content('posts').fetch()
+
+    return { posts }
+  },
+}
 </script>
 
 <style></style>
