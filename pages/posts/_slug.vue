3a73c0ce6d2becb3cbe81f3810e90782515532eb
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-10 18:10:38 +0200

    update: basic stylings for post contnent

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,11 +1,66 @@
 <template>
-  <h1>
-    Hello from post with slug: <code>{{ $route.params }}</code>
-  </h1>
+  <div class="post">
+    <h1>
+      Hello from post with slug: <code>{{ $route.params }}</code>
+    </h1>
+
+    <!-- TODO: add table of contents (https://content.nuxtjs.org/snippets#table-of-contents) -->
+    <!-- {{ post.toc }} -->
+
+    <hr />
+
+    <nuxt-content :document="post" />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content, params }) {
+    const post = await $content('posts', params.slug).fetch()
+
+    return { post }
+  },
+}
 </script>
 
-<style></style>
+<style lang="scss">
+.post {
+  padding: 4rem 4vw;
+
+  .nuxt-content-container {
+    font-size: calc(12px + 0.4vw);
+    line-height: 1.65;
+
+    // Code stylings
+    .nuxt-content-highlight {
+      position: relative;
+    }
+
+    code {
+      font-family: 'IBM Plex Mono';
+    }
+
+    .filename {
+      position: absolute;
+      top: 0;
+      right: 0;
+
+      color: whitesmoke;
+
+      padding: 0.2rem 0.75rem;
+      border-radius: 0 0.3rem 0 0.3rem;
+
+      background-color: hsla(0, 50%, 50%, 0.125);
+    }
+
+    // Heading links
+    a[aria-hidden='true'] {
+      margin-right: 0.5rem;
+    }
+
+    .icon.icon-link::before {
+      content: '#';
+    }
+  }
+}
+</style>
