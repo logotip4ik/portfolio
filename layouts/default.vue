6b0cbca23b14679d20eb54034b5bbae57a730121
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-11 19:01:50 +0200

    update: this need for smooth scrollbar working properly

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
+  <smooth-scrollbar id="scroll-area">
     <Navbar />
-    <Nuxt />
-  </div>
+    <Nuxt id="scroll-content" />
+  </smooth-scrollbar>
 </template>
+
+<style>
+#scroll-area.is-pc {
+  width: 100%;
+  height: 100vh;
+}
+
+#scroll-content.is-pc {
+  width: 100%;
+  height: max-content;
+}
+</style>
