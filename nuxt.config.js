606dd2750a0e0ab9aea58df64034402c19e9d4fd
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-16 18:36:16 +0200

    update: maybe i really do not need that smooth scroll ?

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,9 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/smooth-scroll.js', mode: 'client' }],
+  plugins: [
+    // { src: '~/plugins/smooth-scroll.js', mode: 'client' }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
