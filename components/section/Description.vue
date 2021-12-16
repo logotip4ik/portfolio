da9ad494bbd34186c4c67343a9aa96e1bd3df62e
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-16 20:03:32 +0200

    update: added new component and added cool scroll transition

---
+++ components/section/Description.vue
@@ -0,0 +1,73 @@
+<template>
+  <section ref="section" class="section">
+    <p class="section__description">
+      <span
+        v-for="(char, key) in projectDescriptionChars"
+        :key="key"
+        ref="sectionDescriptionChars"
+        class="section__description__char"
+        v-html="char"
+      ></span>
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: { project: { type: Object, required: true, default: () => ({}) } },
+  computed: {
+    projectDescriptionChars() {
+      return this.project.description.split('')
+    },
+  },
+  mounted() {
+    const gsap = this.$gsap
+    const { section, sectionDescriptionChars } = this.$refs
+
+    const charsTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: 'top center-=100px',
+        end: 'bottom+=50% center',
+        scrub: 0.25,
+      },
+    })
+
+    for (const char of sectionDescriptionChars) {
+      charsTl.to(char, {
+        stagger: 0.1,
+        onUpdate: () => {
+          const classFunction =
+            charsTl.scrollTrigger.direction < 0 ? 'remove' : 'add'
+          char.classList[classFunction]('section__description__char--active')
+        },
+      })
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.section {
+  &__description {
+    font-size: calc(2rem + 2vw);
+    line-height: 1.75;
+    text-align: left;
+
+    -webkit-text-stroke: 1px var(--surface-color);
+
+    @supports (-webkit-text-stroke: 1px var(--surface-color)) {
+      color: var(--ff-color);
+    }
+
+    &__char {
+      transition: color var(--transition-time)
+        var(--transition-function-ease-out);
+
+      &--active {
+        color: var(--surface-color);
+      }
+    }
+  }
+}
+</style>
