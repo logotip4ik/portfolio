e04625318093335a711c6abdceb8ed628a323e48
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-16 20:14:16 +0200

    update: basic layout of project page

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,11 +1,192 @@
 <template>
-  <h1>
-    Hell from projects with slug: <code>{{ $route.params }}</code>
-  </h1>
+  <div ref="project" class="project">
+    <header ref="projectHeader" class="project__header">
+      <h2 ref="projectHeaderName" class="project__header__name">
+        <span v-for="(words, i) in projectNameWords" :key="i">
+          {{ words }}
+        </span>
+      </h2>
+
+      <ul v-if="project.source || project.live" class="project__header__links">
+        <a
+          v-if="project.live"
+          :href="project.live"
+          class="project__header__links__link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Live
+        </a>
+        <a
+          v-if="project.source"
+          :href="project.source"
+          class="project__header__links__link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source
+        </a>
+      </ul>
+
+      <Blob
+        v-for="i in 2"
+        :key="i"
+        ref="projectHeaderBlobs"
+        :delay-to-appear="1"
+        :repeat-duration="15"
+      />
+    </header>
+    <sectionDescription
+      ref="projectSections"
+      :project="project"
+      class="project__section"
+    />
+    <sectionDescription
+      ref="projectSections"
+      :project="project"
+      class="project__section"
+    />
+    <!-- <section ref="projectSections" :key="i + 1" class="project__section">
+        {{ project.description }}
+      </section>
+      <section ref="projectSections" :key="i + 2" class="project__section">
+        {{ project.description }}
+      </section>
+      <section ref="projectSections" :key="i + 3" class="project__section">
+        {{ project.description }}
+      </section> -->
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content, params }) {
+    const project = await $content('projects/' + params.slug).fetch()
+
+    return { project }
+  },
+  computed: {
+    projectNameWords() {
+      return this.project.name.split(' ')
+    },
+  },
+  mounted() {
+    const gsap = this.$gsap
+
+    const { projectHeaderName, projectHeader } = this.$refs
+    const projectSections = gsap.utils.toArray('.project__section')
+
+    gsap.fromTo(
+      projectHeaderName.children,
+      { yPercent: 100, opacity: 0 },
+      { yPercent: 0, opacity: 1, delay: 0.25, duration: 0.75, stagger: 0.05 }
+    )
+
+    gsap.fromTo(
+      projectHeaderName,
+      { y: -50 },
+      {
+        y: 400,
+        scrollTrigger: {
+          id: 1,
+          scrub: 1.25,
+          trigger: projectHeader,
+          start: 'top top',
+          end: 'bottom top',
+          invalidateOnRefresh: true,
+        },
+      }
+    )
+
+    projectSections.forEach((section, i) => {
+      gsap.set(section, {
+        backgroundColor:
+          (i + 1) % 2 ? 'var(--ff-color)' : 'var(--surface-color)',
+        color: (i + 1) % 2 ? 'var(--surface-color)' : 'var(--ff-color)',
+      })
+
+      gsap.to(section, {
+        scrollTrigger: {
+          id: 'scrollbar-for-section-' + i,
+          trigger: section,
+          start: 'top top',
+          pin: true,
+          invalidateOnRefresh: true,
+        },
+      })
+    })
+  },
+}
 </script>
 
-<style></style>
+<style lang="scss">
+.project {
+  position: relative;
+  isolation: isolate;
+
+  &__header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: relative;
+    z-index: -1;
+
+    min-height: 90vh;
+    padding: 0 5vw;
+
+    &__name {
+      display: inline-block;
+
+      font-size: calc(3rem + 0.5vw);
+      text-align: center;
+
+      max-width: 15ch;
+      overflow: hidden;
+
+      span {
+        display: inline-block;
+        margin-right: 1rem;
+
+        opacity: 0;
+      }
+    }
+
+    &__links {
+      display: flex;
+      justify-content: flex-start;
+      align-items: flex-start;
+      flex-direction: column;
+
+      position: absolute;
+      z-index: -1;
+      bottom: 6rem;
+      left: clamp(2rem, 2vw, 5rem);
+
+      padding-left: 0;
+
+      &__link {
+        font-size: calc(1.75rem + 0.33vw);
+        margin: 0.25rem 0;
+
+        &::before {
+          content: '↗';
+        }
+      }
+    }
+  }
+
+  &__section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: relative;
+    z-index: 10;
+
+    width: 100%;
+    height: 100vh;
+    padding: 2vw 5vw;
+  }
+}
+</style>
