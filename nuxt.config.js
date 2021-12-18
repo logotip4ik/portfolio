d170e126f650910880752a41f2ddda806bdde45c
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-18 23:15:31 +0200

    update: added some body atters that was needed for proper loader anim

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,9 @@ export default {
     htmlAttrs: {
       lang: 'en',
     },
+    bodyAttrs: {
+      'data-initial-load': true,
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
