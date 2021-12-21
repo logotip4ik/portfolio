8d4d548686fa7e8e7b84dc47d043c9b7eb68d39c
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-21 21:46:07 +0200

    update: added normalize css to nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/css/global.css'],
+  css: ['@/assets/css/global.css', 'normalize.css/normalize.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
