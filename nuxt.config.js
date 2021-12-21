916971b4848f055d4dc032551a5f6bd6eb3de3b5
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-22 00:09:11 +0200

    update: added loco scroll plugin to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   css: ['@/assets/css/global.css', 'normalize.css/normalize.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/loco-scroll.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
