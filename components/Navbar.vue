2ce1dfdc03ce699755d2e6d2370f1764db4845d5
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-21 22:01:13 +0200

    update: added basic navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar__navigation">
+      <li
+        v-for="(link, key) in links"
+        :key="key"
+        class="navbar__navigation__item"
+      >
+        {{ link.label }}
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent',
+  data: () => ({
+    links: [{ label: 'Projects' }, { label: 'About' }, { label: 'Contact' }],
+  }),
+}
+</script>
+
+<style lang="scss">
+.navbar {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  width: 100%;
+  padding: 1rem var(--pd-x) 0;
+
+  &__navigation {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    list-style-type: none;
+
+    &__item + &__item {
+      margin-left: 3vw;
+
+      cursor: pointer;
+    }
+  }
+}
+</style>
