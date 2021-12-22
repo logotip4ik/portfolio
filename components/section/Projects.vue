9c8c4fe3e69e2964e19928bfe94bcc7452079f79
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-22 14:52:40 +0200

    update: added blob stylings and upped a bit title font size

--- components/section/Projects.vue
+++ components/section/Projects.vue
@@ -4,6 +4,12 @@
     <ul class="projects__list">
       <ProjectItem v-for="key in 3" :key="key" />
     </ul>
+
+    <img
+      src="/img/blob-2.png"
+      alt="floating blob"
+      class="projects__blob parallax-blob"
+    />
   </section>
 </template>
 
@@ -15,16 +21,31 @@ export default {
 
 <style lang="scss">
 .projects {
+  position: relative;
+  isolation: isolate;
+
   padding: 0 var(--pd-x);
 
   &__title {
     font-weight: 500;
-    font-size: clamp(1.25rem, 5vw, 2.125rem);
+    font-size: clamp(1.25rem, 5vw, 2.25rem);
     margin-bottom: 3rem;
   }
 
   &__list {
     padding-left: clamp(1rem, 5vw, 3rem);
+  }
+
+  &__blob {
+    position: absolute;
+    top: 35%;
+    left: 0;
+    z-index: -1;
+
+    width: clamp(10rem, 25vw, 35rem);
+    height: auto;
+
+    filter: blur(35px);
   }
 }
 </style>
