e29b6421f057e7689c17a9b5e5c27bbb2e5b8156
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-22 14:40:47 +0200

    update: added basic component for project item and basic project section

---
+++ components/ProjectItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <li class="project">
+    <div class="project__content">
+      <h3 class="project__content__title">LPNU StudRecruit</h3>
+      <hr class="project__content__hr" />
+      <ul class="project__content__desc-list">
+        <li v-for="j in 5" :key="j" class="project__content__desc-list__item">
+          Next.js,
+        </li>
+      </ul>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.project {
+  margin-bottom: 1.5rem;
+
+  &::marker {
+    content: '/ ' counter(list-item) '  ';
+    font-variant-numeric: diagonal-fractions;
+  }
+
+  &__content {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    position: relative;
+
+    &__title {
+      display: inline-block;
+      font-size: calc(1.5rem + 0.33vw);
+      font-weight: 500;
+    }
+
+    &__hr {
+      display: inline-block;
+
+      flex: 1;
+
+      width: 100%;
+      margin-left: 2rem;
+      border-color: #fff;
+    }
+
+    &__desc-list {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+
+      position: absolute;
+      top: 60%;
+      right: 0;
+
+      padding: 0;
+      list-style-type: none;
+
+      &__item {
+        display: inline-block;
+
+        font-size: calc(0.75rem + 0.3vw);
+        color: var(--ff-secondary-color);
+
+        & + & {
+          margin-left: 0.5rem;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ components/section/Projects.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="projects">
+    <h2 class="projects__title">/ Projects</h2>
+    <ul class="projects__list">
+      <ProjectItem v-for="key in 3" :key="key" />
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionProjects',
+}
+</script>
+
+<style lang="scss">
+.projects {
+  padding: 0 var(--pd-x);
+
+  &__title {
+    font-weight: 500;
+    font-size: clamp(1.25rem, 5vw, 2.125rem);
+    margin-bottom: 3rem;
+  }
+
+  &__list {
+    padding-left: clamp(1rem, 5vw, 3rem);
+  }
+}
+</style>
