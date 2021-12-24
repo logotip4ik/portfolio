43126da1c0c04119b0641cd67ad4a69566d66dbe
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-24 19:38:15 +0200

    update: added asscroll plugin to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     // '@/plugins/loco-scroll.client.js'
+    '@/plugins/asscroll.client.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
