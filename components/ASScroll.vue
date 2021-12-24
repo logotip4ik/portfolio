84a990b8e316590fc422f3db027e298f41aa2a1f
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-24 19:37:42 +0200

    update: addded component for smooth scroll

---
+++ components/ASScroll.vue
@@ -0,0 +1,72 @@
+<template>
+  <div asscroll-container class="scroller">
+    <slot asscroll />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    asscroll: null,
+  }),
+  beforeMount() {
+    this.initSmoothScroll()
+
+    // this.$nuxt.on('asscroll:scroll-to', (number) =>
+    //   this.asscroll?.scrollTo(number)
+    // )
+  },
+  beforeUpdate() {
+    this.$gsap.ticker.remove(this.asscroll.update)
+  },
+  methods: {
+    initSmoothScroll() {
+      const ASScroll = this.ASScroll
+
+      const asscroll = new ASScroll({
+        disableRaf: true,
+      })
+
+      this.$gsap.ticker.add(asscroll.update)
+
+      this.$ScrollTrigger.defaults({
+        scroller: asscroll.containerElement,
+      })
+
+      this.$ScrollTrigger.scrollerProxy(asscroll.containerElement, {
+        scrollTop(value) {
+          if (arguments.length) {
+            asscroll.currentPos = value
+            return
+          }
+          return asscroll.currentPos
+        },
+        getBoundingClientRect() {
+          return {
+            top: 0,
+            left: 0,
+            width: window.innerWidth,
+            height: window.innerHeight,
+          }
+        },
+        fixedMarkers: true,
+      })
+
+      asscroll.on('update', this.$ScrollTrigger.update)
+      this.$ScrollTrigger.addEventListener('refresh', asscroll.resize)
+
+      this.$nextTick(() => {
+        asscroll.enable({
+          newScrollElements: document.querySelectorAll(
+            '.gsap-marker-start, .gsap-marker-end, [asscroll]'
+          ),
+        })
+      })
+
+      this.asscroll = asscroll
+    },
+  },
+}
+</script>
+
+<style></style>
