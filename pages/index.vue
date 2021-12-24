ee85727f6986f69126695c1e81922f4a86185634
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-12-24 22:31:08 +0200

    update: moved loco scroll component up to layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,25 @@
 <template>
-  <LocoScroll
-    ref="scroller"
-    :getted-options="{
-      direction: 'vertical',
-      smartphone: {
-        direction: 'vertical',
-      },
-      tablet: {
-        direction: 'vertical',
-      },
-    }"
-    @init="locomotive = $refs.scroller.locomotive"
-  >
-    <SectionHeader data-scroll-section />
+  <div>
+    <SectionHeader />
     <main>
-      <SectionProjects data-scroll-section />
-      <SectionAbout data-scroll-section />
+      <SectionProjects />
+      <SectionAbout />
     </main>
-  </LocoScroll>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  data: () => ({
-    locomotive: null,
-  }),
-  watch: {
-    locomotive: {
-      handler() {
-        this.locomotive.on('scroll', this.$ScrollTrigger.update)
-
-        const locomotive = this.locomotive
-
-        this.$ScrollTrigger.scrollerProxy(locomotive.el, {
-          scrollTop(value) {
-            return arguments.length
-              ? locomotive.scrollTo(value, 0, 0)
-              : locomotive.scroll.instance.scroll.y
-          },
-          getBoundingClientRect() {
-            return {
-              top: 0,
-              left: 0,
-              width: window.innerWidth,
-              height: window.innerHeight,
-            }
-          },
-          pinType: document.querySelector('.js-locomotive').style.transform
-            ? 'transform'
-            : 'fixed',
-        })
-      },
-    },
-  },
   mounted() {
     const blobs = document.querySelectorAll('.parallax-blob')
     const gsap = this.$gsap
-
-    console.log(this.$refs.scroller.locomotive.el)
 
     gsap.to(blobs, {
       y: (i) => gsap.utils.random(-300, -100) * (i + 1),
       invalidateOnRefresh: true,
       scrollTrigger: {
         scrub: 2,
-        scroller: this.$refs.scroller.locomotive.el,
         start: 'top top',
         end: 'bottom top',
       },
