7ee5f00ac490fb87f13ac2a3a8c41d13d1234727
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-03 22:30:17 +0200

    update: added global css to nuxt globals

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['normalize.css/normalize.css'],
+  css: ['~assets/css/global.css', 'normalize.css/normalize.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
