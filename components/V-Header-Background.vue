e32ffeac23b3810bdf56b0d88225b365e5f1c40a
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-04 12:13:08 +0200

    update: added separate file for three js scene and added some stuff for ssr to work

---
+++ components/V-Header-Background.vue
@@ -0,0 +1,142 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script>
+import * as THREE from 'three'
+
+import fragmentShader from '~/assets/shaders/fragment.glsl'
+import vertexShader from '~/assets/shaders/vertex.glsl'
+import { AberrationShader } from '~/assets/shaders/CustomPass.js'
+
+let EffectComposer
+let RenderPass
+let UnrealBloomPass
+let ShaderPass
+
+// NOTE: This actually enables to turn on ssr in nuxt and don't even try to wrap it into client only component
+// cuz it isn't fixing the issue. The component still is transpiled on a server which is causing the error
+if (process.browser) {
+  EffectComposer =
+    require('three/examples/jsm/postprocessing/EffectComposer.js').EffectComposer
+  RenderPass =
+    require('three/examples/jsm/postprocessing/RenderPass.js').RenderPass
+  UnrealBloomPass =
+    require('three/examples/jsm/postprocessing/UnrealBloomPass.js').UnrealBloomPass
+  ShaderPass =
+    require('three/examples/jsm/postprocessing/ShaderPass.js').ShaderPass
+}
+
+export default {
+  data: () => ({
+    camera: null,
+    scene: null,
+    renderer: null,
+    composer: null,
+    circle: null,
+    clock: null,
+  }),
+  mounted() {
+    const { canvas } = this.$refs
+
+    // THREE: Scene
+    this.scene = new THREE.Scene()
+
+    // THREE: Renderer
+    this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+    this.renderer.setPixelRatio(window.devicePixelRatio || 2)
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setClearColor(0x000000, 1)
+    this.renderer.physicallyCorrectLights = true
+    this.renderer.outputEncoding = THREE.sRGBEncoding
+
+    // THREE: Camera
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    )
+
+    this.camera.position.set(0, 0, 2)
+
+    // THREE: Composer
+    const renderPass = new RenderPass(this.scene, this.camera)
+
+    const bloomPass = new UnrealBloomPass(
+      new THREE.Vector2(window.innerWidth, window.innerHeight),
+      0.75,
+      0.5,
+      0.125
+    )
+    const AberrationShaderPass = new ShaderPass(AberrationShader)
+
+    this.composer = new EffectComposer(this.renderer)
+    this.composer.addPass(renderPass)
+    this.composer.addPass(bloomPass)
+    this.composer.addPass(AberrationShaderPass)
+
+    // THREE: Object
+    const geometry = new THREE.CircleBufferGeometry(
+      // 1.25 / window.devicePixelRatio,
+      (window.innerWidth / 1500) * (window.devicePixelRatio || 2),
+      // window.innerWidth / 1500,
+      128
+    )
+
+    const material = new THREE.ShaderMaterial({
+      fragmentShader,
+      vertexShader,
+      extensions: {
+        derivatives: '#extension GL_OES_standard_derivatives : enable',
+      },
+      uniforms: {
+        time: { value: 0.0, type: 'f' },
+        resolution: {
+          value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+        },
+      },
+      depthTest: false,
+      depthWrite: false,
+      blending: THREE.AdditiveBlending,
+      transparent: true,
+    })
+    this.circle = new THREE.Mesh(geometry, material)
+
+    // THREE: Adding to scene
+    this.scene.add(this.circle)
+
+    // THREE: Prep
+    this.clock = new THREE.Clock()
+
+    this.resize()
+    window.addEventListener('resize', this.resize)
+    // NOTE: try to use only one requestAnimationFrame
+    // this will improve overall performance
+    this.$gsap.ticker.add(this.render)
+  },
+  methods: {
+    resize() {
+      const width = window.innerWidth
+      const height = window.innerHeight
+
+      this.renderer.setSize(width, height)
+      this.composer.setSize(width, height)
+      this.camera.aspect = width / height
+
+      this.camera.updateProjectionMatrix()
+
+      // NOTE: this will help for performance
+      this.camera.matrixAutoUpdate = false
+      this.camera.updateMatrix()
+
+      this.circle.matrixAutoUpdate = false
+      this.circle.updateMatrix()
+    },
+    render() {
+      this.circle.material.uniforms.time.value = this.clock.getElapsedTime()
+      this.composer.render()
+    },
+  },
+}
+</script>
--- components/V-Header.vue
+++ components/V-Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
-    <canvas ref="canvas" class="header__canvas"></canvas>
+    <V-Header-Background class="header__canvas" />
     <div class="header__container">
       <h1 class="header__container__title">
         Bogdan<br /><span class="ml-responsive serif">Kostyuk</span>
@@ -11,128 +11,7 @@
 </template>
 
 <script>
-import * as THREE from 'three'
-
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
-
-import fragmentShader from '~/assets/shaders/fragment.glsl'
-import vertexShader from '~/assets/shaders/vertex.glsl'
-import { AberrationShader } from '~/assets/shaders/CustomPass.js'
-
-export default {
-  data: () => ({
-    camera: null,
-    scene: null,
-    renderer: null,
-    composer: null,
-    circle: null,
-    clock: null,
-  }),
-  mounted() {
-    const { canvas } = this.$refs
-
-    // THREE: Scene
-    this.scene = new THREE.Scene()
-
-    // THREE: Renderer
-    this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
-    this.renderer.setPixelRatio(window.devicePixelRatio || 2)
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
-    this.renderer.setClearColor(0x000000, 1)
-    this.renderer.physicallyCorrectLights = true
-    this.renderer.outputEncoding = THREE.sRGBEncoding
-
-    // THREE: Camera
-    this.camera = new THREE.PerspectiveCamera(
-      70,
-      window.innerWidth / window.innerHeight,
-      0.001,
-      1000
-    )
-
-    this.camera.position.set(0, 0, 2)
-
-    // THREE: Composer
-    const renderPass = new RenderPass(this.scene, this.camera)
-
-    const bloomPass = new UnrealBloomPass(
-      new THREE.Vector2(window.innerWidth, window.innerHeight),
-      0.75,
-      0.5,
-      0.125
-    )
-    const AberrationShaderPass = new ShaderPass(AberrationShader)
-
-    this.composer = new EffectComposer(this.renderer)
-    this.composer.addPass(renderPass)
-    this.composer.addPass(bloomPass)
-    this.composer.addPass(AberrationShaderPass)
-
-    // THREE: Object
-    const geometry = new THREE.CircleBufferGeometry(
-      // 1.25 / window.devicePixelRatio,
-      (window.innerWidth / 1500) * (window.devicePixelRatio || 2),
-      // window.innerWidth / 1500,
-      128
-    )
-
-    const material = new THREE.ShaderMaterial({
-      fragmentShader,
-      vertexShader,
-      extensions: {
-        derivatives: '#extension GL_OES_standard_derivatives : enable',
-      },
-      uniforms: {
-        time: { value: 0.0, type: 'f' },
-        resolution: {
-          value: new THREE.Vector2(window.innerWidth, window.innerHeight),
-        },
-      },
-      depthTest: false,
-      depthWrite: false,
-      blending: THREE.AdditiveBlending,
-      transparent: true,
-    })
-    this.circle = new THREE.Mesh(geometry, material)
-
-    // THREE: Adding to scene
-    this.scene.add(this.circle)
-
-    // THREE: Prep
-    this.clock = new THREE.Clock()
-
-    this.resize()
-    // NOTE: try to use one requestAnimationFrame
-    // this will improve overall performance
-    this.$gsap.ticker.add(this.render)
-  },
-  methods: {
-    resize() {
-      const width = window.innerWidth
-      const height = window.innerHeight
-
-      this.renderer.setSize(width, height)
-      this.composer.setSize(width, height)
-      this.camera.aspect = width / height
-
-      this.camera.updateProjectionMatrix()
-
-      // NOTE: this will help for performance
-      this.camera.matrixAutoUpdate = false
-      this.camera.updateMatrix()
-
-      this.circle.matrixAutoUpdate = false
-      this.circle.updateMatrix()
-    },
-    render() {
-      this.circle.material.uniforms.time.value = this.clock.getElapsedTime()
-      this.composer.render()
-    },
-  },
-}
+export default {}
 </script>
 
 <style lang="scss">
@@ -142,6 +21,7 @@ export default {
 
   width: 100%;
   min-height: 100vh;
+  background-color: #0e0d0d;
 
   &__canvas {
     position: absolute;
