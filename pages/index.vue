a2984988f83d5b5e83e083b7b2e138b00914432f
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-05 15:37:55 +0200

    update: added some todos and added just scroll content

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,14 @@
   <div>
     <V-Navbar></V-Navbar>
     <V-Header></V-Header>
+    <div class="spacer"></div>
+    <!-- TODO: add project section -->
+    <!-- NOTE: project section should probably scroll horizontally -->
+    <!-- TODO: add about section -->
+    <!-- TODO: add contact section -->
+    <!-- NOTE: projects, about and contact should me in a main tag, footer should be separate -->
+    <!-- TODO: add footer section -->
+    <!-- NOTE: footer section should have some sort of reveal animation as user is scrolling -->
   </div>
 </template>
 
@@ -10,3 +18,9 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style>
+.spacer {
+  min-height: 200vh;
+}
+</style>
