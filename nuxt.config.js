87c63ac83afd267cb9b21ee07cb137d5d3d1961e
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-09 21:49:43 +0200

    update: added custom plugin for smooth scroll

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~plugins/smooth-scroll.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/smooth-scroll.client.js
@@ -0,0 +1,39 @@
+import ASScroll from '@ashthornton/asscroll'
+
+export default ({ $gsap, $ScrollTrigger }, inject) => {
+  const el = document.querySelector('#__layout')
+
+  const asscroll = new ASScroll({
+    disableRaf: true,
+    containerElement: el,
+  })
+
+  $gsap.ticker.add(asscroll.update)
+
+  $ScrollTrigger.defaults({
+    scroller: asscroll.containerElement,
+  })
+
+  $ScrollTrigger.scrollerProxy(asscroll.containerElement, {
+    scrollTop(value) {
+      return arguments.length
+        ? (asscroll.currentPos = value)
+        : asscroll.currentPos
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }
+    },
+  })
+
+  asscroll.on('update', $ScrollTrigger.update)
+  $ScrollTrigger.addEventListener('refresh', asscroll.resize)
+
+  inject('asscroll', asscroll)
+
+  window.addEventListener('DOMContentLoaded', () => asscroll.enable())
+}
