e29e58c0070c74159e3a3418adc50cbb25037805
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-09 19:25:59 +0200

    update: added basic about section

---
+++ components/V-About.vue
@@ -0,0 +1,47 @@
+<template>
+  <section ref="section" class="about">
+    <h2 class="about__title serif">About</h2>
+    <nuxt-content :document="about" class="about__text"></nuxt-content>
+  </section>
+</template>
+
+<script>
+export default {
+  props: { about: { type: Object, required: true, default: () => ({}) } },
+  mounted() {
+    const { section } = this.$refs
+
+    const gsap = this.$gsap
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        scrub: 1,
+      },
+    })
+
+    tl.to('body', { backgroundColor: '#0e0d0d' })
+    tl.to('body', { backgroundColor: '#fff' })
+  },
+}
+</script>
+
+<style lang="scss">
+.about {
+  padding: 1rem clamp(1rem, 7vw, 5rem) 8rem;
+  min-height: 100vh;
+  color: #dfdfdf;
+
+  &__title {
+    text-align: center;
+  }
+
+  &__text {
+    p {
+      font-size: var(--step--1);
+      line-height: 1.75;
+      letter-spacing: 0.1px;
+    }
+  }
+}
+</style>
