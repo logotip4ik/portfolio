d2cfeeeca50cd77ed7cc3a2ad11c18a204dab7af
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-10 18:30:46 +0200

    update: added basic layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <V-Navbar></V-Navbar>
+    <div class="scroller">
+      <Nuxt></Nuxt>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style></style>
