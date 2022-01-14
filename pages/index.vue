0c31f767dde94bda70547ff7e431534d976d663f
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-14 16:35:00 +0200

    update: removed some todos

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,11 @@
       <V-Work></V-Work>
       <V-About></V-About>
     </main>
-    <div class="spacer"></div>
-    <!-- TODO: add about section -->
     <!-- TODO: add contact section -->
-    <!-- NOTE: projects, about and contact should me in a main tag, footer should be separate -->
     <!-- TODO: add footer section -->
     <!-- NOTE: footer section should have some sort of reveal animation as user is scrolling -->
     <!-- NOTE: footer ideas: https://www.rezo-zero.com -->
     <!-- TODO: add and see if floating particles will suite the site, like here: https://www.details.co.jp/ -->
-    <!-- BUG: on computer, scrolling gsap is also somehow messing around cuz then the scroller is a bit buggy and jumpy -->
   </div>
 </template>
 
