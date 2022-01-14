ba0ab8e9a8915b89a2b4b6171a36aebd3bc68ded
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-14 14:50:09 +0200

    update: added separate component for paralax h2

---
+++ components/V-H2.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2 ref="title" class="title-h2 serif"><slot></slot></h2>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const { title } = this.$refs
+
+    const gsap = this.$gsap
+
+    gsap.fromTo(
+      title,
+      { yPercent: 50 },
+      {
+        yPercent: -50,
+        scrollTrigger: {
+          scrub: 0.75,
+          trigger: title.parentElement,
+          start: 'top bottom',
+          end: '+=50% top',
+        },
+      }
+    )
+  },
+}
+</script>
+
+<style>
+.title-h2 {
+  text-align: center;
+}
+</style>
