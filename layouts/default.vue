8058771c70b93490cc1c12e40496bd3e01cab60f
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-16 18:04:46 +0200

    update: added current section handler to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,13 +5,28 @@
       <Nuxt></Nuxt>
     </div>
     <V-Loader></V-Loader>
-    <V-Menu></V-Menu>
+    <V-Menu :current-section="currentSection"></V-Menu>
   </div>
 </template>
 
 <script>
 export default {
   name: 'DefaultLayout',
+  data: () => ({ currentSection: 'home' }),
+  mounted() {
+    const gsap = this.$gsap
+    const ScrollTrigger = this.$ScrollTrigger
+
+    gsap.utils.toArray('header, section').forEach((item, i) => {
+      ScrollTrigger.create({
+        trigger: item,
+        start: '1% 45%',
+        end: 'bottom 45%',
+        onEnter: () => (this.currentSection = i),
+        onEnterBack: () => (this.currentSection = i),
+      })
+    })
+  },
 }
 </script>
 
