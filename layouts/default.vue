81dec29fd4ca06912a64e4618010893e9a04eadc
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-21 22:37:53 +0200

    update: added greeting logs

--- layouts/default.vue
+++ layouts/default.vue
@@ -33,6 +33,7 @@ export default {
       })
     })
 
+    this.logGreeting()
     this.setViewHeight()
     window.addEventListener('resize', this.setViewHeight)
   },
@@ -40,6 +41,18 @@ export default {
     setViewHeight() {
       const vh = window.innerHeight * 0.01
       document.documentElement.style.setProperty('--vh', `${vh}px`)
+    },
+    logGreeting() {
+      // eslint-disable-next-line
+      console.log(
+        '%cBogdan Kostyuk',
+        'background-color: #030303;border-radius: 0.125rem;padding: 5px 10px;font-family: "Times New Roman", serif;font-size: 2rem'
+      )
+      // eslint-disable-next-line
+      console.log(
+        "%cHello😁 curios friend! Currently I am looking👀 for a job as a front end developer, so write let's get in touch! If you are looking for a source📦 here you are: https://github.com/logotip4ik/portfolio/tree/v4-pika",
+        'background-color: #030303;border-radius: 0.125rem;padding: 5px 10px;font-size: 1.1rem;line-height:1.75'
+      )
     },
   },
 }
