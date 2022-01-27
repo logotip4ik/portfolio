beffb956da7a050c62ef21d385f642900f195096
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-27 22:06:09 +0200

    update: added new module into nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,6 +63,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '~modules/cloudflare-headers.js',
+    '~modules/update-humans-txt.js',
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://google-fonts.nuxtjs.org
