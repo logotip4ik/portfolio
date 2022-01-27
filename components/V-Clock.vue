65b0a09c396febc8fadd4030c5d8539d36287857
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-27 17:01:05 +0200

    update: again changed clock appearance

--- components/V-Clock.vue
+++ components/V-Clock.vue
@@ -1,13 +1,17 @@
 <template>
   <p ref="clock" class="clock">
+    <span aria-hidden="true"><GlobeSVG></GlobeSVG>{{ localTime }}</span>
+    <br />
+    <span aria-hidden="true">Ukraine</span>
     <span class="sr-only">{{ `time in Ukraine ${localTime}` }}</span>
-    <span aria-hidden="true">Kyiv</span>
-    <span aria-hidden="true">{{ localTime }}</span>
   </p>
 </template>
 
 <script>
+import GlobeSVG from '~/assets/img/globe.svg?inline'
+
 export default {
+  components: { GlobeSVG },
   data() {
     return {
       localTime: this.getLocalTime(),
@@ -30,18 +34,28 @@ export default {
 
 <style lang="scss">
 .clock {
+  --base-font-size: calc(var(--step--1) - 0.075rem);
+
   position: absolute;
 
   color: lighten(black, 20);
   opacity: 0.95;
   margin: 0;
-  font-size: calc(var(--step--1) - 0.075rem);
+  font-size: var(--base-font-size);
+  line-height: 1.25;
+  text-align: center;
 
-  span:nth-child(2) {
+  svg {
+    transform: translateY(19%);
+
+    max-width: var(--step-0);
+    height: auto;
+  }
+
+  span:nth-of-type(2) {
     display: inline-block;
-
-    margin-inline-start: 0.25rem;
-    // transform: translateY(-1px);
+    font-size: calc(var(--base-font-size) + 0.13rem);
+    transform: translateX(1px);
   }
 }
 </style>
