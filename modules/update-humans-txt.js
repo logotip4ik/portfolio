8dbfa92b09d9f6beb7cb0a6a8733bc3e866b7493
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2022-01-27 22:05:57 +0200

    update: added module for injecting last update date into humans file

---
+++ modules/update-humans-txt.js
@@ -0,0 +1,26 @@
+import path from 'path'
+import fs from 'fs'
+import consola from 'consola'
+
+const logger = consola.withScope('nuxt:update-humans-txt')
+const lastBuildTime = new Date().toDateString()
+
+export default function () {
+  const self = this
+
+  const humansTxtDistPath = path.join(
+    self.nuxt.options.generate.dir,
+    'humans.txt'
+  )
+
+  self.nuxt.hook('generate:done', () => {
+    if (!fs.existsSync(humansTxtDistPath))
+      return logger.error(
+        "No `humans.txt` was found, won't inject last update time"
+      )
+
+    fs.appendFileSync(humansTxtDistPath, `\n\tLast update: ${lastBuildTime}`)
+
+    logger.success('Injected last update time into `humans.txt`')
+  })
+}
