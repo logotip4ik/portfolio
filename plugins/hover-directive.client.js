b8ee2dc6c908da4e43e96afaa4810b8eccaeaea5
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-02-07 17:10:40 +0200

    fix: mounting event listeners twice on one element

--- plugins/hover-directive.client.js
+++ plugins/hover-directive.client.js
@@ -3,10 +3,12 @@ import Vue from 'vue'
 export default () => {
   const emitFactory = (name, value) => () => window.$nuxt.$emit(name, value)
 
-  Vue.directive('hoverable', (node) => {
-    node.style.cursor = 'none'
+  Vue.directive('hoverable', {
+    bind: (node) => {
+      node.style.cursor = 'none'
 
-    node.addEventListener('mouseenter', emitFactory('toggle-hovering'))
-    node.addEventListener('mouseleave', emitFactory('toggle-hovering'))
+      node.addEventListener('mouseenter', emitFactory('toggle-hovering'))
+      node.addEventListener('mouseleave', emitFactory('toggle-hovering'))
+    },
   })
 }
