d784fd7c2895f02906387a7cb3019ef36b365d33
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-02-07 13:28:29 +0200

    update: added a bit more space at the top of even cards

--- components/V-Work.vue
+++ components/V-Work.vue
@@ -47,7 +47,7 @@ export default {
 
     @media screen and(min-width: 798px) {
       &__item:nth-child(even) {
-        margin-block-start: 2rem;
+        margin-block-start: 4rem;
       }
     }
   }
