4a379603f2fce4ed6e5cc408bc01716147cf1ce5
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-05-08 22:37:07 +0300

    update: added new emitter listeners and now svgs in pointer has good looking appear and change animation

--- components/V-Pointer.vue
+++ components/V-Pointer.vue
@@ -1,9 +1,21 @@
 <script setup>
+import LinkSVG from '~/assets/img/arrow-link.svg';
+import OuterLinkSVG from '~/assets/img/arrow-outer-link.svg';
+
+import { pointerModifiersWhitelist } from '~/lib/constants';
+
+const SVGComponents = {
+  [pointerModifiersWhitelist.at(0)]: LinkSVG,
+  [pointerModifiersWhitelist.at(1)]: OuterLinkSVG,
+};
+
 const { gsap } = useGsap();
+const emitter = useEmitter();
 
 let firstMove = false;
 
 const pointer = ref(null);
+const pointerState = ref('');
 
 onMounted(() => {
   gsap.set(pointer.value, {
@@ -32,14 +44,36 @@ onMounted(() => {
     toPointerX(x);
     toPointerY(y);
   });
+
+  for (const modifier of pointerModifiersWhitelist) {
+    emitter.on(`pointer:${modifier}:active`, () => {
+      pointerState.value = modifier;
+    });
+    emitter.on(`pointer:${modifier}:inactive`, () => {
+      pointerState.value = '';
+    });
+  }
 });
 </script>
 
 <template>
-  <div ref="pointer" class="pointer"></div>
+  <div
+    ref="pointer"
+    :class="{ pointer: true, 'pointer--active': pointerState !== '' }"
+  >
+    <Transition name="pointer-svg">
+      <Component
+        :is="SVGComponents[pointerState]"
+        :key="pointerState"
+        class="pointer__svg"
+      />
+    </Transition>
+  </div>
 </template>
 
 <style lang="scss">
+$ease-back-out: cubic-bezier(0.34, 1.56, 0.64, 1);
+
 .pointer {
   --size: 1rem;
 
@@ -58,5 +92,35 @@ onMounted(() => {
 
   pointer-events: none;
   transform: translate(-50%, -50%);
+
+  transition: width 400ms $ease-back-out, height 400ms $ease-back-out;
+
+  &--active {
+    --size: 5.5rem;
+  }
+
+  &__svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    width: 35%;
+    height: 35%;
+
+    color: var(--surface-color);
+
+    transform: translate(-50%, -50%) rotate(0) scale(1);
+  }
+}
+
+.pointer-svg-enter-active,
+.pointer-svg-leave-active {
+  transition: transform 300ms $ease-back-out;
+  transition-delay: 100ms;
+}
+
+.pointer-svg-enter-from,
+.pointer-svg-leave-to {
+  transform: translate(-50%, -50%) rotate(45deg) scale(0);
 }
 </style>
