53dfeece642f31bc73e0cbf07bb8b8771c038af9
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-05-13 13:30:38 +0300

    update: added reference to current section idx needed for navbar

---
+++ composables/use-current-section.js
@@ -0,0 +1,1 @@
+export default () => useState('current-section', () => 0);
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,30 @@
 <script setup>
 const { $smoothScroll } = useNuxtApp();
+const { gsap, ScrollTrigger } = useGsap();
+
+const currentSection = useCurrentSection();
 
 onMounted(() => {
   $smoothScroll.disable();
+
+  // website parts, heading, sections and footer
+  const parts = gsap.utils.toArray('header, section, footer');
+
+  parts.forEach((part, key) => {
+    ScrollTrigger.create({
+      trigger: part,
+      start: 'top 55%',
+      end: 'bottom 55%',
+      onEnter: () => (currentSection.value = key),
+      onEnterBack: () => (currentSection.value = key),
+    });
+  });
 });
 </script>
 
 <template>
   <div>
+    <VNavbar />
     <div class="smooth-scroll-fix">
       <VHeader />
       <main>
