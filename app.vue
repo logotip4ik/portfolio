ac06c30d02fc9160c7fa39550eb919ec868985da
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-05-20 14:03:32 +0300

    update: added some page hooks for updating the smooth scroll and disabling the pointer

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup>
+const { $smoothScroll, ...nuxtApp } = useNuxtApp();
+const emitter = useEmitter();
+
+nuxtApp.hook('page:start', () => emitter.emit('pointer:inactive'));
+nuxtApp.hook('page:finish', () => setTimeout($smoothScroll.update, 50));
+</script>
+
 <template>
   <div>
     <div id="scroller">
