b0c6194942721c539f1770fe63b888b606d1a88a
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-05-22 16:12:54 +0300

    update: added global event listener for setting manualy 1 vh size

--- app.vue
+++ app.vue
@@ -4,6 +4,24 @@ const emitter = useEmitter();
 
 nuxtApp.hook('page:start', () => emitter.emit('pointer:inactive'));
 nuxtApp.hook('page:finish', () => setTimeout($smoothScroll.update, 50));
+
+function setVh() {
+  const windowHeight = window.innerHeight;
+  document.documentElement.setAttribute(
+    'style',
+    `--vh: ${windowHeight / 100}px`
+  );
+}
+
+onMounted(() => {
+  setVh();
+
+  window.addEventListener('resize', setVh);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', setVh);
+});
 </script>
 
 <template>
