bdd281b5d4cda2b8de6062445edabee85d6ed899
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-05-22 16:14:33 +0300

    update: added global greetings for curios people

--- app.vue
+++ app.vue
@@ -13,7 +13,21 @@ function setVh() {
   );
 }
 
+function logGreeting() {
+  // eslint-disable-next-line
+  console.log(
+    '%cBogdan Kostyuk',
+    'background-color: #030303;border-radius: 0.125rem;padding: 5px 10px;font-family: "Times New Roman", serif;font-size: 2rem;color: white'
+  );
+  // eslint-disable-next-line
+  console.log(
+    "%cHello😁 curios friend! Currently I am looking👀 for a job as a front end developer, so let's get in touch! If you are looking for a source📦 here you are: https://github.com/logotip4ik/portfolio",
+    'background-color: #030303;border-radius: 0.125rem;padding: 5px 10px;font-size:1rem;color: white;line-height:1.75'
+  );
+}
+
 onMounted(() => {
+  logGreeting();
   setVh();
 
   window.addEventListener('resize', setVh);
