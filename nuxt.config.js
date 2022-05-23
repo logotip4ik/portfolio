547fc433e076c9f59c70ce00351bd2a9772d5430
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-05-23 13:44:50 +0300

    update: added even more seo for the website

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,10 @@ export default defineNuxtConfig({
   head: {
     htmlAttrs: { lang: 'en', dir: 'ltr' },
     meta: [
+      { title: 'Bogdan Kostyuk | Front End Developer' },
+      { lang: 'en' },
+      { language: 'English' },
+      { property: 'name', name: 'name', content: 'Bogdan Kostyuk' },
       { charset: 'utf-8' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
       { 'http-equiv': 'Reply-to', content: 'contact@bogdankostyuk.xyz' },
@@ -24,11 +28,43 @@ export default defineNuxtConfig({
       },
       { name: 'format-detection', content: 'telephone=no' },
       { name: 'robots', content: 'all' },
-      { name: 'theme-color', content: 'var(--black-color)' },
+      { name: 'theme-color', content: 'var(--surface-color)' },
       { name: 'apple-mobile-web-app-capable', content: 'yes' },
       { name: 'copyrighted-site-verification', content: 'c552f044f4e41c2b' },
+      { property: 'url', name: 'url', content: 'https://bogdankostyuk.xyz' },
+      {
+        property: 'og:site_name',
+        name: 'og:site_name',
+        content: 'Bogdan Kostyuk',
+      },
+      {
+        property: 'og:title',
+        name: 'og:title',
+        content: 'Front End Developer',
+      },
+      {
+        property: 'og:description',
+        name: 'og:description',
+        content:
+          'I will build you website as quick as possible and with high attention to details рџ‘Њ',
+      },
+      { property: 'og:locale', name: 'og:locale', content: 'en' },
+      {
+        property: 'og:url',
+        name: 'og:url',
+        content: 'https://bogdankostyuk.xyz',
+      },
+      { property: 'og:type', name: 'og:type', content: 'website' },
+      {
+        property: 'og:image',
+        name: 'og:image',
+        content: 'https://bogdankostyuk.xyz/logo.png',
+      },
+      { property: 'og:image:width', name: 'og:image:width', content: '1440' },
+      { property: 'og:image:height', name: 'og:image:height', content: '1440' },
     ],
     link: [
+      { rel: 'canonical', href: 'https://bogdankostyuk.xyz/' },
       { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', sizes: 'any' },
       {
