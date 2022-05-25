50607227fe7e2a5f0577680c04a4679b7bb977e5
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-05-25 23:57:05 +0300

    update: a bit changed sizing and positioning of flag stripe

--- components/UkraineFlagStripe.vue
+++ components/UkraineFlagStripe.vue
@@ -14,12 +14,12 @@
   bottom: 0;
   z-index: 5;
 
-  width: calc(var(--step-5));
+  width: calc(var(--step-5) + 1.25rem);
   height: 100vh;
 
   background: linear-gradient(180deg, #2659ac 50%, #f8d447 50%);
 
-  transform: translateX(calc(-100% + var(--step--2)));
+  transform: translateX(calc(-100% + var(--step--2) - 0.075rem));
 
   transition: transform 400ms ease;
 
