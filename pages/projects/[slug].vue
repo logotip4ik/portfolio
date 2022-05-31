b34a64131167b96a8b30b46595752420254b283f
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-05-31 23:08:01 +0300

    update: added a template page for project

---
+++ pages/projects/[slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="projects-page">
+    {{ $route.name }}
+  </div>
+</template>
+
+<style lang="scss">
+.projects-page {
+  min-height: 100vh;
+
+  padding: 1.25rem 5vw;
+}
+</style>
