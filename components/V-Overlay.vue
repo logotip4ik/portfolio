aa74e7a2fe526f23bd32b1fc0d2e70dea67a537a
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-01 22:09:38 +0300

    update: removed some not needed styles

--- components/V-Overlay.vue
+++ components/V-Overlay.vue
@@ -23,9 +23,6 @@
     right: 0;
     bottom: 0;
 
-    opacity: 0;
-    pointer-events: none;
-
     &:nth-of-type(1) {
       background-color: var(--primary-color);
       background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23999999' fill-opacity='0.075'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
