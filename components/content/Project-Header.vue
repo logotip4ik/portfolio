4d4b035b77be45e668b177bc104bc24655cae0cd
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-02 00:45:30 +0300

    update: added some basic components for project pages

---
+++ components/content/Project-Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="project-header" data-scroll-section>
+    <slot />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.project-header {
+  padding: 25vh 10vw 5vh;
+}
+</style>
