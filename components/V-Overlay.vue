917eff865c57c831fb457921de22f732b366c8eb
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-01 18:12:34 +0300

    update: somewhat styled overlay

---
+++ components/V-Overlay.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="page-overlay">
+    <div class="page-overlay__slide"></div>
+    <div class="page-overlay__slide"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.page-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 10;
+
+  pointer-events: none;
+
+  &__slide {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    opacity: 0;
+    pointer-events: none;
+
+    &:nth-of-type(1) {
+      background-color: var(--primary-color);
+      background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23999999' fill-opacity='0.075'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+
+      z-index: 1;
+    }
+
+    &:nth-of-type(2) {
+      background-color: var(--surface-color);
+
+      z-index: 2;
+    }
+  }
+}
+</style>
