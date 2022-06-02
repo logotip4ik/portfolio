60ce466ce2dfde182bdd4a4fb19557d35c233ced
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-02 13:06:01 +0300

    update: a bit changed layout and added space for links

--- components/content/Project-Header.vue
+++ components/content/Project-Header.vue
@@ -1,11 +1,37 @@
 <template>
   <header class="project-header" data-scroll-section>
-    <slot />
+    <slot class="project-header__title" />
+
+    <div class="project-header__info">
+      <div class="project-header__info__links">
+        <slot class="project-header__info__links__live" name="live" />
+        <slot class="project-header__info__links__source" name="source" />
+      </div>
+    </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
 .project-header {
-  padding: 25vh 10vw 5vh;
+  padding: 30vh clamp(1rem, 7vw, 10rem) 5vh;
+
+  &__info {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+
+    margin-top: 2rem;
+
+    &__links {
+      display: flex;
+      justify-content: flex-end;
+      align-items: flex-start;
+      flex-direction: column;
+
+      text-align: right;
+
+      margin-left: auto;
+    }
+  }
 }
 </style>
