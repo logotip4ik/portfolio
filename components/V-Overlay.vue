b7fe87028d869af673e8330a8f9862b35592297f
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-02 22:26:35 +0300

    update: changed background color for overlay

--- components/V-Overlay.vue
+++ components/V-Overlay.vue
@@ -26,8 +26,12 @@
     &:nth-of-type(1) {
       z-index: 1;
 
-      background-color: var(--primary-color);
-      background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23999999' fill-opacity='0.075'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+      // background-color: hsl(343 20% 35%);
+      background-color: #8e8285;
+
+      @media (prefers-color-scheme: light) {
+        background-color: var(--primary-color);
+      }
     }
 
     &:nth-of-type(2) {
