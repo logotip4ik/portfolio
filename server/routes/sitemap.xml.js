3d7cb7a42e78dcade575f6a5f7b58390c79069b3
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-02 22:51:16 +0300

    update: added sitemap xml route for the website

---
+++ server/routes/sitemap.xml.js
@@ -0,0 +1,21 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { serverQueryContent } from '#content/server';
+
+export default defineEventHandler(async (event) => {
+  const defaults = { lastmod: new Date().toISOString(), changefreq: 'weekly' };
+
+  // Fetching all the documents
+  const docs = await serverQueryContent(event).find();
+
+  const sitemap = new SitemapStream({ hostname: 'https://bogdankostyuk.xyz' });
+
+  sitemap.write({ url: '/', ...defaults });
+
+  for (const doc of docs)
+    if (doc._path.includes('projects'))
+      sitemap.write({ url: doc._path, ...defaults });
+
+  sitemap.end();
+
+  return streamToPromise(sitemap);
+});
