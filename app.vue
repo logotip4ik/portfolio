4e2e6561f11773105c39be054a5a97f1d1e73572
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-02 22:29:08 +0300

    update: changed font of messaged logged in to the console

--- app.vue
+++ app.vue
@@ -93,7 +93,7 @@ function logGreeting() {
   // eslint-disable-next-line
   console.log(
     '%cBogdan Kostyuk',
-    'background-color: #030303;border-radius: 0.125rem;padding: 5px 10px;font-family: "Times New Roman", serif;font-size: 2rem;color: white'
+    'background-color: #030303;border-radius: 0.125rem;padding: 5px 10px;font-family: "Arial", sans-serif;font-size: 2rem;color: white'
   );
   // eslint-disable-next-line
   console.log(
