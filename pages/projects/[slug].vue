3f0771f1656e850308f019684d45a10f677b7c45
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-14 17:39:22 +0300

    update: added appear anim for nav back link

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,11 +1,30 @@
 <script setup>
 const route = useRoute();
+
 const { data: project } = await useAsyncData(
   `project-${route.params.slug}`,
   () => queryContent(`projects/${route.params.slug}`).findOne()
 );
 
+const { gsap } = useGsap();
+const emitter = useEmitter();
+
 useSeoHead({ title: project.value.title });
+
+onMounted(() => {
+  const showBackButton = () =>
+    gsap.fromTo(
+      '.nav__back-link',
+      { autoAlpha: 0 },
+      { autoAlpha: 1, delay: 0.5 }
+    );
+
+  emitter.on('overlay:hiding', showBackButton);
+
+  onBeforeUnmount(() => {
+    emitter.off('overlay:hiding', showBackButton);
+  });
+});
 </script>
 
 <template>
