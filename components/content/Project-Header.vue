fcc73406c2781037e501a798b7676d1ff76909b4
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-18 15:49:25 +0300

    update: added reveal animations for info links

--- components/content/Project-Header.vue
+++ components/content/Project-Header.vue
@@ -1,17 +1,48 @@
+<script setup>
+const { gsap } = useGsap();
+const emitter = useEmitter();
+
+onMounted(() => {
+  emitter.once('overlay:hiding', () => {
+    gsap.to('.project-header__info__links__item__content', {
+      yPercent: -110,
+      stagger: { from: 'end', each: 0.075 },
+      ease: 'expo.out',
+      duration: 1,
+      delay: 0.225,
+    });
+  });
+});
+</script>
+
 <template>
   <header class="project-header" data-scroll-section>
     <slot class="project-header__title" />
 
     <div class="project-header__info">
-      <div class="project-header__info__links">
-        <slot class="project-header__info__links__live" name="live" />
-        <slot class="project-header__info__links__source" name="source" />
-      </div>
+      <ul class="project-header__info__links">
+        <li
+          v-if="$slots.live"
+          class="project-header__info__links__item project-header__info__links__item--live"
+        >
+          <span class="project-header__info__links__item__content">
+            <slot name="live" />
+          </span>
+        </li>
+        <li
+          v-if="$slots.source"
+          class="project-header__info__links__item project-header__info__links__item--source"
+        >
+          <span class="project-header__info__links__item__content">
+            <slot name="source" />
+          </span>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .project-header {
   padding: 30vh clamp(1rem, 7vw, 10rem) 5vh;
 
@@ -23,14 +54,27 @@
     margin-top: 2rem;
 
     &__links {
-      display: flex;
-      justify-content: flex-end;
-      align-items: flex-start;
-      flex-direction: column;
+      display: grid;
+      justify-content: start;
+      align-content: end;
+      grid-template-rows: 1fr;
+      gap: 0.5rem;
 
       text-align: right;
 
+      padding: 0;
       margin-left: auto;
+      list-style-type: none;
+
+      &__item {
+        overflow: hidden;
+
+        &__content {
+          display: inline-block;
+
+          transform: translateY(110%);
+        }
+      }
     }
   }
 }
