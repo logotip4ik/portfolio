b6b0b2929f9bf7e71b41f9591fb74d12b79cdd72
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-18 15:48:56 +0300

    update: added new method to emitter

--- composables/use-emitter.js
+++ composables/use-emitter.js
@@ -1,5 +1,16 @@
 export default () => {
   const nuxtApp = useNuxtApp();
 
-  return nuxtApp.$emitter;
+  return {
+    ...nuxtApp.$emitter,
+    once: (eventName, cb) => {
+      const handler = () => {
+        if (typeof cb === 'function') cb();
+
+        nuxtApp.$emitter.off(eventName, handler);
+      };
+
+      nuxtApp.$emitter.on(eventName, handler);
+    },
+  };
 };
