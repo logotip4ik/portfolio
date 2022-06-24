5758ff62e8cb6f658c2f938fd165fb9ac69dbc70
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-24 22:30:05 +0300

    update: added basic test for index page

---
+++ cypress/e2e/index-page.cy.ts
@@ -0,0 +1,57 @@
+const menuSocialLinks = ['linkedin', 'telegram', 'github', 'email'];
+const footerSocialLinks = [
+  'linkedinlinkedin',
+  'telegramtelegram',
+  'githubgithub',
+  'emailemail',
+];
+
+describe('Index page', () => {
+  it('Loader hides', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.loader').should('not.be.visible');
+  });
+
+  it('Page title is visible', () => {
+    cy.get('.header__container__title__line__content').should('be.visible');
+  });
+
+  it('Projects count is greater then zero', () => {
+    cy.get('.project').should('have.length.at.least', 0);
+  });
+
+  it('Social links in menu is correct', () => {
+    const links = cy.get('.menu__back-item__content__links__item');
+
+    links.each((link, i) => {
+      cy.wrap(link).should('have.text', menuSocialLinks[i]);
+    });
+  });
+
+  it('Social links in footer is correct', () => {
+    const links = cy.get('.footer__content__social-links__link');
+
+    links.each((link, i) => {
+      cy.wrap(link).should('have.text', footerSocialLinks[i]);
+    });
+  });
+
+  it('Scrolling down to projects', () => {
+    cy.get('.nav__list__item').contains('Projects').click({ timeout: 8000 });
+
+    cy.get('.project').should('be.visible');
+  });
+
+  it('Scrolling down to about me', () => {
+    cy.get('.nav__list__item').contains('About').click({ timeout: 8000 });
+
+    const aboutMe = cy.get('.about-me__content');
+
+    aboutMe.should('be.visible');
+
+    aboutMe.each((el) => {
+      expect(el.text().length).to.be.greaterThan(0);
+    });
+  });
+});
