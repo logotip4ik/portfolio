817b76d27234b486b32e08c6018640704b9adeec
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-10 13:37:31 +0300

    refactor: moved head to app

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,83 +12,94 @@ export default defineNuxtConfig({
 
   // nighter head or vueMeta is not working, idk why,
   // but global seo currently is placed in index page
-  head: {
-    htmlAttrs: { lang: 'en', dir: 'ltr' },
-    meta: [
-      { title: 'Bogdan Kostyuk | Front End Developer' },
-      { lang: 'en' },
-      { language: 'English' },
-      { property: 'name', name: 'name', content: 'Bogdan Kostyuk' },
-      { charset: 'utf-8' },
-      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-      { 'http-equiv': 'Reply-to', content: 'contact@bogdankostyuk.xyz' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content:
-          'I will build you website as quick as possible and with high attention to details👌',
-      },
-      { name: 'format-detection', content: 'telephone=no' },
-      { name: 'robots', content: 'all' },
-      { name: 'theme-color', content: 'var(--surface-color)' },
-      { name: 'apple-mobile-web-app-capable', content: 'yes' },
-      { name: 'copyrighted-site-verification', content: 'c552f044f4e41c2b' },
-      { property: 'url', name: 'url', content: 'https://bogdankostyuk.xyz' },
-      {
-        property: 'og:site_name',
-        name: 'og:site_name',
-        content: 'Bogdan Kostyuk',
-      },
-      {
-        property: 'og:title',
-        name: 'og:title',
-        content: 'Front End Developer',
-      },
-      {
-        property: 'og:description',
-        name: 'og:description',
-        content:
-          'I will build you website as quick as possible and with high attention to details👌',
-      },
-      { property: 'og:locale', name: 'og:locale', content: 'en' },
-      {
-        property: 'og:url',
-        name: 'og:url',
-        content: 'https://bogdankostyuk.xyz',
-      },
-      { property: 'og:type', name: 'og:type', content: 'website' },
-      {
-        property: 'og:image',
-        name: 'og:image',
-        content: 'https://bogdankostyuk.xyz/logo.png',
-      },
-      { property: 'og:image:width', name: 'og:image:width', content: '1440' },
-      { property: 'og:image:height', name: 'og:image:height', content: '1440' },
-    ],
-    link: [
-      { rel: 'canonical', href: 'https://bogdankostyuk.xyz/' },
-      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', sizes: 'any' },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
-      },
-      { rel: 'manifest', href: '/site.webmanifest' },
-    ],
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en', dir: 'ltr' },
+      meta: [
+        { title: 'Bogdan Kostyuk | Front End Developer' },
+        { lang: 'en' },
+        { language: 'English' },
+        { property: 'name', name: 'name', content: 'Bogdan Kostyuk' },
+        { charset: 'utf-8' },
+        { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+        { 'http-equiv': 'Reply-to', content: 'contact@bogdankostyuk.xyz' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'I will build you website as quick as possible and with high attention to details👌',
+        },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'robots', content: 'all' },
+        { name: 'theme-color', content: 'var(--surface-color)' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'copyrighted-site-verification', content: 'c552f044f4e41c2b' },
+        { property: 'url', name: 'url', content: 'https://bogdankostyuk.xyz' },
+        {
+          property: 'og:site_name',
+          name: 'og:site_name',
+          content: 'Bogdan Kostyuk',
+        },
+        {
+          property: 'og:title',
+          name: 'og:title',
+          content: 'Front End Developer',
+        },
+        {
+          property: 'og:description',
+          name: 'og:description',
+          content:
+            'I will build you website as quick as possible and with high attention to details👌',
+        },
+        { property: 'og:locale', name: 'og:locale', content: 'en' },
+        {
+          property: 'og:url',
+          name: 'og:url',
+          content: 'https://bogdankostyuk.xyz',
+        },
+        { property: 'og:type', name: 'og:type', content: 'website' },
+        {
+          property: 'og:image',
+          name: 'og:image',
+          content: 'https://bogdankostyuk.xyz/logo.png',
+        },
+        { property: 'og:image:width', name: 'og:image:width', content: '1440' },
+        {
+          property: 'og:image:height',
+          name: 'og:image:height',
+          content: '1440',
+        },
+      ],
+      link: [
+        { rel: 'canonical', href: 'https://bogdankostyuk.xyz/' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico',
+          sizes: 'any',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+    },
   },
 
   css: [
@@ -98,11 +109,7 @@ export default defineNuxtConfig({
     '~/assets/styles/global.css',
   ],
 
-  modules: [
-    '@nuxt/content',
-    '@logotip4ik_/nuxt-cloudflare-headers',
-    '@nuxtjs/partytown',
-  ],
+  modules: ['@nuxt/content', '@logotip4ik_/nuxt-cloudflare-headers'],
 
   cloudflareHeaders: {
     '/*': [
