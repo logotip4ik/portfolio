dae711c894cf21192a50dc6a8624e1baa3eaff96
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-23 14:17:44 +0300

    feat: better import filtering

--- plugins/icons.server.js
+++ plugins/icons.server.js
@@ -2,9 +2,13 @@ import path from 'path';
 
 import useIcons from '~/composables/use-icons';
 
-const rawIcons = import.meta.glob('../assets/img/*.svg', { as: 'raw' });
+// NOTE: only svgs ending with .icon.svg will be imported and
+// only them could be used in useIcons composable
+const rawIcons = import.meta.glob('../assets/img/*.icon.svg', {
+  as: 'raw',
+});
 
-/* So, this works by importing svgs as strings and setting it to
+/* So, this works by importing svgs with `iconPrefix` as strings and setting it to
 the useIcons object, which under the hood uses nuxt hook useState.
 This allows to pass data from server to client. We are importing
 svg's on the server and then client can use it vie its key(filename without extension) */
@@ -12,7 +16,7 @@ export default defineNuxtPlugin(() => {
   const icons = useIcons();
 
   for (const key of Object.keys(rawIcons)) {
-    const name = path.parse(key).name;
+    const [name] = path.parse(key).name.split('.');
 
     icons.value[name] = rawIcons[key];
   }
