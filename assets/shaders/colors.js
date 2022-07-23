d834c8313c1b4d85e87bf71b90b620b3df640e3a
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-23 13:31:35 +0300

    refactor: changed data type for easier integration

--- assets/shaders/colors.js
+++ assets/shaders/colors.js
@@ -3,48 +3,52 @@
 // Second color - primary
 // Third color - secondary
 
+// VALUES:
+//   r    g    b
+// [235, 235, 235]
+
 export const WhitePinkGreen = {
   color1: {
-    dark: { x: 0, y: 0, z: 0 },
-    light: { x: 235, y: 235, z: 235 },
+    dark: [0, 0, 0],
+    light: [235, 235, 235],
   },
   color2: {
-    dark: { x: 255, y: 230, z: 237 },
-    light: { x: 255, y: 181, z: 202 },
+    dark: [255, 230, 237],
+    light: [255, 181, 202],
   },
   color3: {
-    dark: { x: 125, y: 179, z: 132 },
-    light: { x: 106, y: 168, z: 114 },
+    dark: [125, 179, 132],
+    light: [106, 168, 114],
   },
 };
 
 export const WhiteGreenPink = {
   color1: {
-    dark: { x: 0, y: 0, z: 0 },
-    light: { x: 235, y: 235, z: 235 },
+    dark: [0, 0, 0],
+    light: [235, 235, 235],
   },
   color2: {
-    dark: { x: 125, y: 179, z: 132 },
-    light: { x: 106, y: 168, z: 114 },
+    dark: [125, 179, 132],
+    light: [106, 168, 114],
   },
   color3: {
-    dark: { x: 255, y: 230, z: 237 },
-    light: { x: 255, y: 181, z: 202 },
+    dark: [255, 230, 237],
+    light: [255, 181, 202],
   },
 };
 
 // Flag of Ukraine
 export const BlueYellowWhite = {
   color1: {
-    dark: { x: 0, y: 87, z: 183 },
-    light: { x: 0, y: 87, z: 183 },
+    dark: [0, 87, 183],
+    light: [0, 87, 183],
   },
   color2: {
-    dark: { x: 255, y: 215, z: 0 },
-    light: { x: 255, y: 215, z: 0 },
+    dark: [255, 215, 0],
+    light: [255, 215, 0],
   },
   color3: {
-    dark: { x: 3, y: 3, z: 3 },
-    light: { x: 235, y: 235, z: 235 },
+    dark: [3, 3, 3],
+    light: [235, 235, 235],
   },
 };
