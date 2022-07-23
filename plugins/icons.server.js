f8076fd2fcb3ac4974af88e17026ee3a82f3fdd5
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-23 14:24:46 +0300

    refactor: finding all svgs and filtering icons with js

--- plugins/icons.server.js
+++ plugins/icons.server.js
@@ -2,22 +2,29 @@ import path from 'path';
 
 import useIcons from '~/composables/use-icons';
 
-// NOTE: only svgs ending with .icon.svg will be imported and
-// only them could be used in useIcons composable
-const rawIcons = import.meta.glob('../assets/img/*.icon.svg', {
+const rawIcons = import.meta.glob('../assets/img/*.svg', {
   as: 'raw',
 });
 
-/* So, this works by importing svgs with `iconPrefix` as strings and setting it to
+/* So, this works by importing svgs as strings and setting it to
 the useIcons object, which under the hood uses nuxt hook useState.
 This allows to pass data from server to client. We are importing
 svg's on the server and then client can use it vie its key(filename without extension) */
+
+/* NOTE: only svgs ending with .icon.svg will be imported
+and only them could be used in useIcons composable */
+
 export default defineNuxtPlugin(() => {
   const icons = useIcons();
 
   for (const key of Object.keys(rawIcons)) {
-    const [name] = path.parse(key).name.split('.');
+    const filename = path.parse(key).name;
+    const splittedFilename = filename.split('.');
 
-    icons.value[name] = rawIcons[key];
+    if (splittedFilename.length < 2 || splittedFilename.at(-1) !== 'icon')
+      continue;
+
+    const iconName = splittedFilename.at(0);
+    icons.value[iconName] = rawIcons[key];
   }
 });
