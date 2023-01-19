15c5968f67243926c895a777b89fbbc71b749aaa
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-01-19 17:46:38 +0200

    refactor: used built in fromTo function to transition button

--- pages/project/[slug].vue
+++ pages/project/[slug].vue
@@ -15,11 +15,9 @@ useSeoHead({ title: project.value.title });
 const projectPage = ref(null);
 
 const showBackButton = () =>
-  gsap.to('.nav__back-link', { autoAlpha: 1, delay: 0.7 });
+  gsap.fromTo('.nav__back-link', { autoAlpha: 0 }, { autoAlpha: 1, delay: 0.7 });
 
 onMounted(() => {
-  gsap.set('.nav__back-link', { autoAlpha: 0 });
-
   imagesLoaded(projectPage.value, () => emitter.emit('images:loaded'));
 });
 
