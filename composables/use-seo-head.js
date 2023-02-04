7e8d16de4c9a4c3d834f086c3a852770d3a5c630
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-02-04 14:21:33 +0200

    refactor: improve seo for sub pages

--- composables/use-seo-head.js
+++ composables/use-seo-head.js
@@ -1,76 +1,85 @@
-export default ({ title, description, link = [], meta = [] }) =>
-  useHead({
-    title,
-    titleTemplate: '%s | Bogdan Kostyuk',
-    htmlAttrs: { lang: 'en', dir: 'ltr' },
-    meta: [
-      { lang: 'en' },
-      { language: 'English' },
-      { property: 'name', name: 'name', content: 'Bogdan Kostyuk' },
-      { charset: 'utf-8' },
-      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-      { 'http-equiv': 'Reply-to', content: 'contact@bogdankostyuk.xyz' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      description ? { name: 'description', content: description } : {},
-      { name: 'format-detection', content: 'telephone=no' },
-      { name: 'robots', content: 'all' },
-      { name: 'theme-color', content: 'var(--surface-color)' },
-      { name: 'apple-mobile-web-app-capable', content: 'yes' },
-      { name: 'copyrighted-site-verification', content: 'c552f044f4e41c2b' },
-      { property: 'url', name: 'url', content: 'https://bogdankostyuk.xyz' },
-      {
-        property: 'og:site_name',
-        name: 'og:site_name',
-        content: 'Bogdan Kostyuk',
-      },
-      { property: 'og:title', name: 'og:title', content: title },
-      description
-        ? {
-            property: 'og:description',
-            name: 'og:description',
-            content: description,
-          }
-        : {},
-      { property: 'og:locale', name: 'og:locale', content: 'en' },
-      {
-        property: 'og:url',
-        name: 'og:url',
-        content: 'https://bogdankostyuk.xyz',
-      },
-      { property: 'og:type', name: 'og:type', content: 'website' },
-      {
-        property: 'og:image',
-        name: 'og:image',
-        content: 'https://bogdankostyuk.xyz/logo.png',
-      },
-      { property: 'og:image:width', name: 'og:image:width', content: '1440' },
-      { property: 'og:image:height', name: 'og:image:height', content: '1440' },
+export default ({ title, description, link = [], meta = [], mode }) => {
+  const base = 'https://bogdankostyuk.xyz';
+  const projectSlug = useRoute().params.slug ?? '';
+  const url = projectSlug ? `${base}/project/${projectSlug}` : base;
 
-      ...meta,
-    ],
-    link: [
-      { rel: 'canonical', href: 'https://bogdankostyuk.xyz/' },
-      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', sizes: 'any' },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
-      },
-      { rel: 'manifest', href: '/site.webmanifest' },
+  useHead(
+    {
+      title,
+      titleTemplate: '%s | Bogdan Kostyuk',
+      htmlAttrs: { lang: 'en', dir: 'ltr' },
+      meta: [
+        { lang: 'en' },
+        { language: 'English' },
+        { property: 'name', name: 'name', content: 'Bogdan Kostyuk' },
+        { charset: 'utf-8' },
+        { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+        { 'http-equiv': 'Reply-to', content: 'contact@bogdankostyuk.xyz' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        description ? { name: 'description', content: description } : {},
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'robots', content: 'all' },
+        { name: 'theme-color', content: 'var(--surface-color)' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'copyrighted-site-verification', content: 'c552f044f4e41c2b' },
+        { property: 'url', name: 'url', content: url },
+        {
+          property: 'og:site_name',
+          name: 'og:site_name',
+          content: 'Bogdan Kostyuk',
+        },
+        { property: 'og:title', name: 'og:title', content: title },
+        description
+          ? {
+              property: 'og:description',
+              name: 'og:description',
+              content: description,
+            }
+          : {},
+        { property: 'og:locale', name: 'og:locale', content: 'en' },
+        { property: 'og:url', name: 'og:url', content: url },
+        { property: 'og:type', name: 'og:type', content: 'website' },
+        {
+          property: 'og:image',
+          name: 'og:image',
+          content: projectSlug
+            ? `${base}/img/${projectSlug}-logo.webp`
+            : `${base}/logo.png`,
+        },
 
-      ...link,
-    ],
-  });
+        ...meta,
+      ],
+      link: [
+        { rel: 'canonical', href: `${url}/` },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico',
+          sizes: 'any',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+
+        ...link,
+      ],
+    },
+    { mode: mode ?? 'server' }
+  );
+};
